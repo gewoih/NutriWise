90d943dfced4645ac29cb1f776b1ed807ed71967
Nikita Ranenko <nranenko@bk.ru>
2024-11-04 21:36:11 +0100

    feat: prompt and chatgpt settings updated

--- src/NutriWise.Infrastructure/OpenAi/OpenAiService.cs
+++ src/NutriWise.Infrastructure/OpenAi/OpenAiService.cs
@@ -9,44 +9,49 @@ namespace NutriWise.Infrastructure.OpenAi;
 
 public class OpenAiService
 {
-	private readonly IConfiguration _configuration;
+    private readonly IConfiguration _configuration;
 
-	public OpenAiService(IConfiguration configuration)
-	{
-		_configuration = configuration;
-	}
+    public OpenAiService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
 
-	public async Task<MealPlanDto> GetMealPlanAsync(double calories, double proteinGrams, double fatGrams,
-		double carbGrams, IEnumerable<ProductDto> availableProducts)
-	{
-		var client = new ChatClient(model: "gpt-4o", apiKey: _configuration["OpenAi:ApiKey"]);
-		var recipeResponseJsonSchema = await EmbeddedResourcesUtils.GetResourceFileContentAsync("recipeResponse.json");
+    public async Task<MealPlanDto> GetMealPlanAsync(int calories, int proteinGrams, int fatGrams, int carbGrams,
+        IEnumerable<ProductDto> availableProducts)
+    {
+        var client = new ChatClient(model: "gpt-4o", apiKey: _configuration["OpenAi:ApiKey"]);
+        var recipeResponseJsonSchema = await EmbeddedResourcesUtils.GetResourceFileContentAsync("recipeResponse.json");
 
-		var options = new ChatCompletionOptions
-		{
-			Temperature = 0.1f,
-			ResponseFormat = ChatResponseFormat.CreateJsonSchemaFormat(
-				jsonSchemaFormatName: "recipes",
-				jsonSchema: BinaryData.FromBytes(Encoding.UTF8.GetBytes(recipeResponseJsonSchema)),
-				jsonSchemaIsStrict: true)
-		};
+        var options = new ChatCompletionOptions
+        {
+            Temperature = 0,
+            ResponseFormat = ChatResponseFormat.CreateJsonSchemaFormat(
+                jsonSchemaFormatName: "recipes",
+                jsonSchema: BinaryData.FromBytes(Encoding.UTF8.GetBytes(recipeResponseJsonSchema)),
+                jsonSchemaIsStrict: true)
+        };
 
-		var availableProductsList =
-			string.Join('\n', availableProducts.Select(product => $"{product.Id} {product.Name}"));
-		
-		var message =
-			$"Generate me daily meals plan with following nutrition value: {calories} calories, " +
-			$"{proteinGrams}g. of proteins, {fatGrams}g. of fats, {carbGrams} of carbs \n\n" +
-			$"Available products List: \n{availableProductsList}";
+        var availableProductsList =
+            string.Join('\n',
+                availableProducts.Select(product =>
+                    $"{product.Id};{product.Name};{product.Calories};{product.Proteins};{product.Fats};{product.Carbs}"));
 
-		var prompt = await EmbeddedResourcesUtils.GetResourceFileContentAsync("generateRecipe.txt");
-		var completion = await client.CompleteChatAsync([$"{prompt} {message}"], options);
-		var response = completion.Value.Content[0].Text;
-		var mealPlanDto = JsonConvert.DeserializeObject<MealPlanDto>(response);
+        var message =
+            $"User's Macronutrient Goals:\n" +
+            $"Calories: {calories}\n" +
+            $"Proteins: {proteinGrams}g.\n" +
+            $"Fats: {fatGrams}g.\n" +
+            $"Carbs: {carbGrams}g.\n\n" +
+            $"Available products List (id;name;calories;proteins;fats;carbs): \n{availableProductsList}";
 
-		if (mealPlanDto?.Meals is null)
-			throw new ApplicationException("Произошла ошибка при генерации плана питания.");
+        var prompt = await EmbeddedResourcesUtils.GetResourceFileContentAsync("generateRecipe.txt");
+        var completion = await client.CompleteChatAsync([$"{prompt}\n\n{message}"], options);
+        var response = completion.Value.Content[0].Text;
+        var mealPlanDto = JsonConvert.DeserializeObject<MealPlanDto>(response);
 
-		return mealPlanDto;
-	}
+        if (mealPlanDto?.Meals is null)
+            throw new ApplicationException("Произошла ошибка при генерации плана питания.");
+
+        return mealPlanDto;
+    }
 }
